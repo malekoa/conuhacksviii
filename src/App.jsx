ba14132ddfd44f99deedefe0715e739469d5c32b
Asha I <85359230+asha97@users.noreply.github.com>
2024-01-20 15:16:19 -0500

    search button and search box front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import Dashboard from "./screens/Dashboard";
-import "./App.css";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import "./App.css"; // Import your CSS file
 
 function App() {
+  const [text, setText] = useState(""); // State to store the entered text
+
+  const handleInputChange = (event) => {
+    setText(event.target.value); // Update the 'text' state when the input changes
+  };
+
   return (
     <div>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <TextField
+            label="Enter Postal Code..."
+            variant="outlined"
+            fullWidth
+            value={text}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained" className="postal-search-btn">
+            Search
+          </Button>
+        </Grid>
+      </Grid>
       <Dashboard />
     </div>
   );
---
+++ src/PostalCodePage.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./screens/Dashboard";
+import "./PostalCodePage.css";
+
+function PostalCodePage() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default PostalCodePage;
