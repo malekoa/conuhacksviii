e999ccfee78f039ce9f7a36dd457aaa765a82d7f
Asha I <85359230+asha97@users.noreply.github.com>
2024-01-20 21:17:50 -0500

    icon environement new dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Dashboard from "./screens/Dashboard";
 import SideBar from "./components/SideBar";
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes instead of Switch
+import EnvironmentSafetyDashboard from "./screens/EnvironmentSafetyDashboard"; 
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="sidebar">
-        <SideBar/>
+    <Router>
+      <div className="app-container">
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="dashboard">
+          <Routes> {/* Use Routes instead of Switch */}
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/environmentsafety" element={<EnvironmentSafetyDashboard />} />
+          </Routes>
+        </div>
       </div>
-
-      <div className="dashboard">
-        <Dashboard/>
-      </div>
-
-    </div>
+    </Router>
   );
 }
 
