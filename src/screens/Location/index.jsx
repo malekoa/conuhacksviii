453f22db05c70ea6021767bd7c56795d1181c905
Laila Alhalabi <laila.alhalabi99@gmail.com>
2024-01-20 22:23:45 -0500

    fixed

---
+++ src/screens/Location/index.jsx
@@ -0,0 +1,29 @@
+import { Grid, Paper } from "@mui/material";
+import HorizontalBarChart from "../../components/HorizontalBarChart";
+import Statistics from "../../components/Statistics";
+import styles from "./Location.styles";
+
+const Location = () => {
+
+  return (
+    <div>
+      <div style={styles.backgroundImg} />
+      <div style={styles.container}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={0} style={styles.card}>
+              <Statistics />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={0} style={styles.card}>
+              <HorizontalBarChart />
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
