3ddd22b4bc3ea402f2d9a9b04704c5d517935f25
Laila Alhalabi <laila.alhalabi99@gmail.com>
2024-01-20 23:12:29 -0500

    created logo

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,11 +6,14 @@ import CloudIcon from "@mui/icons-material/Cloud";
 import PedalBikeIcon from "@mui/icons-material/PedalBike";
 import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import smartHomeIcon from "../../assets/smart_home_icon.png";
 import styles from "./SideBar.styles";
 
 const SideBar = () => {
   return (
     <div style={styles.container}>
+      <img src={smartHomeIcon} alt="logo" width={80} />
+
       <Grid container spacing={8} mt={1} flexDirection={"column"}>
         <Grid item>
           <Link to="/">
